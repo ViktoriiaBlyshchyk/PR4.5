45fe629c05e867122749f4af370056f5dd23c227
ViktoriiaBlyshchyk <viktoriia.blyshchyk.ik.2021@lpnu.ua>
2021-10-11 20:09:17 +0300

    PR4.5

---
+++ PR4.5/PR4.5.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	double x, y, R;
+
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+
+		if (((x * x) + (y * y) < R * R && y < x && x < 0 && y < 0)
+			|| (y > x && (x * x) + (y * y) > R && x > 0 && y > 0))
+
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+
+	cout << endl << fixed;
+
+	for (int i = 0; i < 10; i++)
+	{
+		x = 2. * R * rand() / RAND_MAX - R;
+		y = 2. * R * rand() / RAND_MAX - R;
+
+		if (((x * x) + (y * y) < R * R && y < x && x < 0 && y < 0)
+			|| (y > x && (x * x) + (y * y) > R && x > 0 && y > 0))
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+		else
+			cout << setw(8) << setprecision(4) << x << " "
+			<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+	}
+
+	return 0;
+}
